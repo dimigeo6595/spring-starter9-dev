c98b145083dd1a9e2fc6b016e51a8acdf6f1bf45
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-01 18:33:07 +0200

    feat: add html pages

---
+++ src/main/java/gr/aueb/cf/spring_starter9_dev/controller/HelloController.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.spring_starter9_dev.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller                     //Επιστρέφει HTML
+@RequestMapping("/starter")     //base path του Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("message", "Hello World!");
+        return "index";     //index.html μέσα στον resources/templates folder
+    }
+
+    @GetMapping("/welcome")
+    public String sayWelcome(@RequestParam(name="username", defaultValue = "Guest") String name, Model model){
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
